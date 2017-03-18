867faaa54e9e58f50f72e8f25a4e1ac69d314cb6
John <johncarmichael1984@gmail.com>
2017-03-18 15:24:56 +0100

    full  install command

--- management/index.js
+++ management/index.js
@@ -2,6 +2,10 @@ let cliargs = require('jdc-node-cliarg-reader').readAll();
 
 if( cliargs['install-light'] ){
     require('./install-light');
-} else if( cliargs.help ){
+}
+else if( cliargs['install-full'] ){
+    require('./install-full');
+}
+else if( cliargs.help ){
     require( './help' );
 }
